7145d14fac2c6408cf0380195652f87528a7b09b
Cristian Igreda <cristian.igreda@pucp.edu.pe>
2021-04-27 17:56:20 -0500

    Add few classes

--- GameModel/GameModel.cpp
+++ GameModel/GameModel.cpp
@@ -2,12 +2,56 @@
 
 #include "GameModel.h"
 
-GameModel::GameObject::GameObject()
+GameModel::GameObject::GameObject(){
+    Sprite = new sf::Sprite();
+    Texture = new sf::Texture();
+}
+void GameModel::GameObject::Draw(RenderTarget& rt){
+    rt.draw(*Sprite);
+}
+
+void GameModel::GameObject::Move(float x, float y)
 {
+    Sprite->setPosition(x, y);
+    X = x;
+    Y = y;
+}
+
+GameModel::Beast::Beast(){
+    Name = new sf::String();
+}
+GameModel::Tamer::Tamer(float x, float y){
+    Username = new String();
+    Password = new String();
+    Bag = gcnew GameModel::Bag();
+}
+GameModel::Bag::Bag(){
+    ListBeast = gcnew List<Beast^>();
+}
+GameModel::Store::Store(float x, float y){
+    X = x;
+    Y = y;
+    Salesman = gcnew NPC();
+}
+GameModel::Hospital::Hospital(float x, float y) {
+    X = x;
+    Y = y;
+    Nurse = gcnew NPC();
+}
+
+GameModel::Grass::Grass(float x, float y)
+{
+    Height = TileSize;
+    Length = TileSize;
+    Texture->loadFromFile("Imagenes/Grass.png");
+    Sprite->setTexture(*Texture, true);
+    Sprite->setScale(Length / 500, Height / 500);
+    X = x;
+    Y = y;
     
 }
 
-void GameModel::GameObject::Draw(RenderTarget& rt)
+GameModel::Garden::Garden(float x, float y, int size)
 {
-    rt.draw(*Sprite);
+    throw gcnew System::NotImplementedException();
 }
--- GameModel/GameModel.h
+++ GameModel/GameModel.h
@@ -1,18 +1,82 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-using namespace System;
+const float TileSize = 50;
 using namespace System::Collections::Generic;
 using namespace sf;
 namespace GameModel {
 	public ref class GameObject {
 	public:
-		Sprite* Sprite = new sf::Sprite();
-		Texture* Texture = new sf::Texture();
-		property float Size;
+		property Sprite* Sprite;
+		property Texture* Texture;
+		property float Height;
+		property float Length;
 		property float X;
 		property float Y;
 	public:
 		GameObject();
 		void Draw(RenderTarget& rt);
+		void Move(float x, float y);
+	};
+	public ref class Arena : public GameObject {
+
+	};
+	public ref class Beast : public GameObject {
+	public:
+		property String* Name; // usaremos el String de la libreria SFML
+		property int Type;
+		property int Health;
+		property int Defense;
+		property int Attack;
+		property int Speed;
+		property int Level;
+		property int Exp;
+	public:
+		Beast();
+	};
+	public ref class Bag : public GameObject {
+	public:
+		List<Beast^>^ ListBeast;
+	public:
+		Bag();
+	};
+	public ref class Tamer : public GameObject {
+	public:
+		property String* Username;
+		property String* Password;
+		property int NumBeast;
+		property Bag^ Bag;
+	public:
+		Tamer(float x, float y);
+	};
+	public ref class NPC : public GameObject {
+	public:
+		property int Enemy;
+	};
+	public ref class Store : public GameObject {
+	public:
+		property NPC^ Salesman;
+	public:
+		Store(float x, float y);
+	};
+	public ref class Hospital : public GameObject {
+	public:
+		property NPC^ Nurse;
+	public:
+		Hospital(float x, float y);
+	};
+	public ref class Grass : public GameObject {
+	public:
+		property int SpawnRate;
+	public:
+		Grass(float x,float y);
+	};
+	public ref class Garden {
+	public:
+		property float GrassSize;
+		property float X;
+		property float Y;
+		property int Size;
+	public:
+		Garden(float x, float y, int size);
 	};
 }
