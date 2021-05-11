d90cc63e0407ae2a203e1f5185b025187021e051
GiancarloNievesCortez <giancarlonievescortez@gmail.com>
2021-05-11 16:38:04 -0500

    agragar clase hospital

---
+++ TrueBeastTamer/Hospital.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "Hospital.h"
+
+GameModel::Hospital::Hospital(float x, float y) {
+    Texture = new sf::Texture();
+    Sprite = new sf::Sprite();
+    Texture->loadFromFile("../Imagenes/Hospital.png"); //Esta imagen es de 160 * 140
+    Sprite->setTexture(*Texture, true);
+    X = x;
+    Y = y;
+    Length = 100;
+    Height = 100;
+    Sprite->setScale(Length / 160, Height / 140);
+    Sprite->setPosition(X, Y);
+}
---
+++ TrueBeastTamer/Hospital.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "GameObject.h"
+namespace GameModel {
+public ref class Hospital : public GameObject {//crear ventana
+public:
+	property NPC^ Nurse;
+public:
+	Hospital(float x, float y);
+};
