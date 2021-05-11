dc461c8f13ac10fb0df9dabf6a1ad9f354e2d666
Cristian Igreda <cristian.igreda@pucp.edu.pe>
2021-05-11 16:44:09 -0500

    fix hospital

--- TrueBeastTamer/Hospital.h
+++ TrueBeastTamer/Hospital.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "GameObject.h"
 namespace GameModel {
-public ref class Hospital : public GameObject {//crear ventana
-public:
-	property NPC^ Nurse;
-public:
-	Hospital(float x, float y);
-};
+	public ref class Hospital : public GameObject {//crear ventana
+	public:
+		//property NPC^ Nurse;
+	public:
+		Hospital(float x, float y);
+	};
+}
